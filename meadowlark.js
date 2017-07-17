d6d12b25c7a24fc531e0542c0b06b83f21bf1175
jackie247s <jackie247s@gmail.com>
2017-07-17 13:56:49 +0500

    Added Tests for pages, crosspages, logic and unit testing

--- meadowlark.js
+++ meadowlark.js
@@ -28,6 +28,12 @@ app.get('/about', function(req, res){
       pageTestScript : '/qa/tests-about.js'
     });
 });
+app.get('/tours/hood-river', function(req, res){
+  res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+  res.render('tours/request-group-rate');
+});
 
 // Set middleware for handling incorrect URLs
 app.use(function(req, res, next){
